ac3a668e2facd848b92a01770187c43f2bee321b
Roman Rafikov <romanrara@yandex.ru>
2023-12-14 15:25:40 +0500

    Fix

--- page/sbis_main.py
+++ page/sbis_main.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 class SbisMainPage(BasePage):
     def go_to_contacts_page(self):
-        WebDriverWait(self.browser, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR,
+        WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,
                                                   "#wasaby-content > div > div > div.sbis_ru-Region.bodyContent__zIndex-context > div.sbis_ru-content > div.sbisru-Header-sticky.sbisru-Header--font.sbisru-Header__scheme--default > div.sbisru-Header > div.sbisru-Header__container.sbis_ru-container > ul > li.sbisru-Header__menu-item.sbisru-Header__menu-item-1.mh-8.s-Grid--hide-sm > a")))
         contacts_link = self.browser.find_element(By.CSS_SELECTOR,
                                                   "#wasaby-content > div > div > div.sbis_ru-Region.bodyContent__zIndex-context > div.sbis_ru-content > div.sbisru-Header-sticky.sbisru-Header--font.sbisru-Header__scheme--default > div.sbisru-Header > div.sbisru-Header__container.sbis_ru-container > ul > li.sbisru-Header__menu-item.sbisru-Header__menu-item-1.mh-8.s-Grid--hide-sm > a")
--- page/tensor_main.py
+++ page/tensor_main.py
@@ -10,10 +10,10 @@ class TensorMainPage(BasePage):
                                   '#container > div.tensor_ru-content_wrapper > div > div.tensor_ru-Index__block4-bg > div')
 
     def go_to_tensor_about_link(self):
-        WebDriverWait(self.browser, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR,
+        WebDriverWait(self.browser, 30).until(EC.element_to_be_clickable((By.CSS_SELECTOR,
                                                "#container > div.tensor_ru-content_wrapper > div > div.tensor_ru-Index__block4-bg > div > div > div:nth-child(1) > div > p:nth-child(4) > a")))
         about_link = self.browser.find_element(By.CSS_SELECTOR,
                                                "#container > div.tensor_ru-content_wrapper > div > div.tensor_ru-Index__block4-bg > div > div > div:nth-child(1) > div > p:nth-child(4) > a")
-        WebDriverWait(self.browser, 300).until(EC.element_to_be_clickable(about_link))
+        WebDriverWait(self.browser, 30).until(EC.element_to_be_clickable(about_link))
         self.browser.execute_script("return arguments[0].scrollIntoView(true);", about_link)
         about_link.click()
